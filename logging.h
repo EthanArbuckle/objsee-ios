2ae0b89da3261432fddbf73ca70491d4d53b3821
Ethan Arbuckle <ea@sourcetheorem.com>
2026-01-24 16:28:23 -0800

    use {public} for all oslogging

--- logging.h
+++ logging.h
@@ -8,7 +8,8 @@
 #if __IPHONE_OS_VERSION_MIN_REQUIRED >= 110000
 
 #include <os/log.h>
-#define objsee_log(fmt, ...) os_log(OS_LOG_DEFAULT, fmt, ##__VA_ARGS__)
+
+#define objsee_log(fmt, ...) do { char _log_buf[1024]; snprintf(_log_buf, sizeof(_log_buf), fmt, ##__VA_ARGS__); os_log(OS_LOG_DEFAULT, "%{public}s", _log_buf); } while (0)
 
 #else
 
